d2e9967d87d20ee2ae5e813095f3c64c393d24d9
Samarth100Coder <kumarsamarth272@gmail.com>
2024-09-14 15:36:33 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def divi(a,b):
+    return a/b
+
+x=int(input('Enter first number: '))
+y=int(input('Enter second number: '))
+print('addition=',add(x,y))
+print('subtraction=',sub(x,y))
+print('multiplication=',mul(x,y))
+print('division=',divi(x,y))
